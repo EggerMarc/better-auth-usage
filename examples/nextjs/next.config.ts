bd5105330a9cea93432aedc73b4324808e3fd0fd
EggerMarc <eggermarc@gmail.com>
2026-03-25 17:45:16 -0300

    chore(example/nextjs): idk anymore, just trying configs to get it to run

--- examples/nextjs/next.config.ts
+++ examples/nextjs/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    turbopack: {
+        resolveAlias: {
+            "package/index": path.resolve(__dirname, "../../dist/index.js"),
+            "package/client": path.resolve(__dirname, "../../dist/client.js"),
+            "package/react": path.resolve(__dirname, "../../dist/react.js"),
+        },
+    },
 };
 
 export default nextConfig;
--- examples/nextjs/src/lib/auth-client.ts
+++ examples/nextjs/src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 import { anonymousClient } from "better-auth/client/plugins"
-import { usageClient } from "@eggermarc/better-auth-usage/client"
+import { usageClient } from "package/client"
 
 export const authClient = createAuthClient({
     baseURL: "http://localhost:3002",
